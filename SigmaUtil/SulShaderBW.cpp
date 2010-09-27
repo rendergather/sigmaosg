0c4a1d4533c76cba55bef5f18afa2d5b9d96611d
marino.peter <marino.peter@7dcd178e-71d3-ac1e-a895-8dd51d562b40>
2010-09-27 13:59:30 +0000

    now possible to get value of invert on shader

--- SigmaUtil/SulShaderBW.cpp
+++ SigmaUtil/SulShaderBW.cpp
@@ -6,6 +6,8 @@
 CSulShaderBW::CSulShaderBW( osg::Node* pNode, osg::Program* pProg  ) :
 CSulShaderBase( pNode )
 {
+	m_bInvert = false;
+
 	if ( !pProg )
 	{
 		addShaderFrag( "shaders/bw.frag" );
@@ -13,11 +15,17 @@ CSulShaderBase( pNode )
 
 		m_rUniform_invert = new osg::Uniform( osg::Uniform::BOOL, "bInvert" );
 		m_rSS->addUniform( m_rUniform_invert.get() );
-		m_rUniform_invert->set( false );
+		m_rUniform_invert->set( m_bInvert );
 	}
 }
 
 void CSulShaderBW::setInvert( bool bInvert )
 {
+	m_bInvert = bInvert;
 	m_rUniform_invert->set( bInvert );
+}
+
+bool CSulShaderBW::isInvert()
+{
+	return m_bInvert;
 }
--- SigmaUtil/SulShaderBW.h
+++ SigmaUtil/SulShaderBW.h
@@ -12,9 +12,11 @@ public:
 			CSulShaderBW( osg::Node* pNode, osg::Program* pProg=0 );
 
 	void	setInvert( bool bInvert );
+	bool	isInvert();
 
 private:
 	osg::ref_ptr<osg::Uniform>	m_rUniform_invert;
+	bool						m_bInvert;
 };
 
 #endif // __SULSHADERBW_H__
