a82d3bf2fb14edb15ec2c22bfe482584ebfc3fa2
oganga@gmail.com <oganga@gmail.com@7dcd178e-71d3-ac1e-a895-8dd51d562b40>
2011-05-06 09:11:36 +0000

    -Merged memleak fix from removecegui

--- trunk/SigmaUtil/SulShaderBrightPass.cpp
+++ trunk/SigmaUtil/SulShaderBrightPass.cpp
@@ -11,15 +11,15 @@ CSulShaderBase( pNode )
 	{
 		addShaderFrag( "shaders/brightpass.frag" );
 		addShaderVert( "shaders/brightpass.vert" );
-
-		m_rUniform_threshold = new osg::Uniform( osg::Uniform::FLOAT, "brightPassThreshold" );
-		m_rUniform_threshold->set( fThreshold );
-		m_rSS->addUniform( m_rUniform_threshold );
-		
-		m_rUniform_falloff= new osg::Uniform( osg::Uniform::FLOAT, "brightPassFalloff" );
-		m_rUniform_falloff->set( 0.5f );
-		m_rSS->addUniform( m_rUniform_falloff );
 	}
+
+	m_rUniform_threshold = new osg::Uniform( osg::Uniform::FLOAT, "brightPassThreshold" );
+	m_rUniform_threshold->set( fThreshold );
+	m_rSS->addUniform( m_rUniform_threshold );
+	
+	m_rUniform_falloff= new osg::Uniform( osg::Uniform::FLOAT, "brightPassFalloff" );
+	m_rUniform_falloff->set( 0.5f );
+	m_rSS->addUniform( m_rUniform_falloff );
 }
 
 void CSulShaderBrightPass::setThreshold( float val )
