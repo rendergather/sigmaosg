7bc01f89f13f26f70bbfefb490db84b1cd143615
marino.peter <marino.peter@7dcd178e-71d3-ac1e-a895-8dd51d562b40>
2011-08-29 11:58:08 +0000

    cleanup

--- SigmaUtil/SulIntersectGraph.cpp
+++ SigmaUtil/SulIntersectGraph.cpp
@@ -9,34 +9,34 @@ CSulIntersectGraph::CSulIntersectGraph()
 	m_rLineSeg = new osg::LineSegment;
 }
 
-void CSulIntersectGraph::SetGraph( osg::Node* pNode )
+void CSulIntersectGraph::setGraph( osg::Node* pNode )
 {
 	m_rGraph = pNode;
 }
 
-void CSulIntersectGraph::Reset()
+void CSulIntersectGraph::reset()
 {
 	m_iv->reset();
 }
 
-void CSulIntersectGraph::SetLine( const osg::Vec3& v0, const osg::Vec3& v1 )
+void CSulIntersectGraph::setLine( const osg::Vec3& v0, const osg::Vec3& v1 )
 {
 	m_rLineSeg->set( v0, v1 );
 }
 
-osg::LineSegment* CSulIntersectGraph::GetLine()
+osg::LineSegment* CSulIntersectGraph::getLine()
 {
 	return m_rLineSeg.get();
 }
 
-bool CSulIntersectGraph::Update()
+bool CSulIntersectGraph::update()
 {
 	if ( !m_rGraph.valid() )
 	{
 		return false;
 	}
 
-	Reset();
+	reset();
 	m_iv->addLineSegment( m_rLineSeg.get() );
 	m_rGraph->accept( *m_iv );
 
@@ -48,14 +48,14 @@ bool CSulIntersectGraph::Update()
 	return false;
 }
 
-osg::Vec3 CSulIntersectGraph::GetWorldIntersectPoint()
+osg::Vec3 CSulIntersectGraph::getWorldIntersectPoint()
 {
 	osgUtil::IntersectVisitor::HitList hitList = m_iv->getHitList( m_rLineSeg.get() );
 	return hitList[0].getWorldIntersectPoint();
 }
 
 
-osgUtil::Hit CSulIntersectGraph::GetHit()
+osgUtil::Hit CSulIntersectGraph::getHit()
 {
 	osgUtil::IntersectVisitor::HitList hitList = m_iv->getHitList( m_rLineSeg.get() );
 	return hitList[0];
