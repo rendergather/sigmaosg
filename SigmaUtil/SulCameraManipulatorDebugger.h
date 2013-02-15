b9a4b2ebc52ed0fc0304c32ef1ae87e5c96249f8
marino.peter <marino.peter@gmail.com>
2013-02-15 12:30:07 +0000

    debug manipulator work in progress

--- SigmaUtil/SulCameraManipulatorDebugger.h
+++ SigmaUtil/SulCameraManipulatorDebugger.h
@@ -5,6 +5,7 @@
 
 #include <osgGA/CameraManipulator>
 #include <osgGA/OrbitManipulator>
+#include <osgManipulator/Projector>
 
 class CSulCameraManipulatorDebugger : public osgGA::CameraManipulator
 {
@@ -12,7 +13,7 @@ public:
 	CSulCameraManipulatorDebugger();
 
 protected:
-	virtual bool			getHitPoint( osg::Vec3& hit );
+	virtual bool			calcHitPoint( const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& aa, osg::Vec3d& hit );
 
 	////////////////////////////////////////////////////////////////
 	// from base class (abstract methods)
@@ -24,8 +25,22 @@ protected:
 	virtual osg::Matrixd	getInverseMatrix() const;
 
 private:
-	bool					handle( const osgGA::GUIEventAdapter& ea,osgGA::GUIActionAdapter& aa );
-	bool					handleMousePush( const osgGA::GUIEventAdapter& ea,osgGA::GUIActionAdapter& aa );
+	bool					handle( const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& aa );
+	bool					handleMouseDrag( const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& aa );
+	bool					handleMousePush( const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& aa );
+	bool					handleMouseRelease( const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& aa );
+
+private:
+	osg::Vec3d										m_hit;
+
+	bool											m_bMousePush;
+	float											m_mouseLastX;
+	float											m_mouseLastY;
+
+	osg::ref_ptr<osgManipulator::PlaneProjector>	m_planeProjector;
+
+//	osg::Vec3										m_center;
+	osg::Quat										m_rotation;
 };
 
 #endif // __SULCAMERAMANIPULATORDEBUGGER_H__
