dbd72d5022db0baddf65c71acb831359a8a758c1
marino.peter@gmail.com <marino.peter@gmail.com@7dcd178e-71d3-ac1e-a895-8dd51d562b40>
2013-04-23 12:02:14 +0000

    work in progress

--- trunk/osghelp/HelloJavaDLL/HelloJavaDLL.cpp
+++ trunk/osghelp/HelloJavaDLL/HelloJavaDLL.cpp
@@ -4,9 +4,30 @@
 /*
 	Notes: 
 
-		javac test.java
-		javah test
+		1. compile the java class
+			javac test.java
 
+		2. create the c++ header code
+			javah test
+
+		3. compile the c++ dll
+
+		4. create a jar file that contains both the class and dll
+			jar cf test.jar test.java Debug/HelloJavaDLL.dll
+
+		5. create a unique key
+			keytool -genkey -keystore test.keystore -alias signkey -keypass password -storepass password -keyalg RSA -validity 365 -dname "CN=John Doe, OU=Development, O=ACME"
+
+		6. sign the jar file with the unique key generated in step 5
+			jarsigner -keystore test.keystore -storepass password -keypass password test.jar signkey
+
+
+
+		http://www.java-tips.org/other-api-tips/jni/simple-example-of-using-the-java-native-interface.html
+		http://vkessels.home.xs4all.nl/articles/jnijarapplet.html
+
+		perhaps:
+		http://www.codeproject.com/Articles/791/Embed-OpenGL-inside-Java-AWT-Canvas
 
 */
 
@@ -17,9 +38,9 @@
 #include <stdio.h>
 
 JNIEXPORT void JNICALL
-Java_test_myout( JNIEnv* env, jobject obj )
+Java_test_print( JNIEnv* env, jobject obj )
 {
-	printf("Hello World from C++\n");
+	printf( "Hello World from C++ the new one\n" );
 	return;
 }
 
