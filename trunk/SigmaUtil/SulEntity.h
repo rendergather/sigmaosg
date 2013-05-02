7abcdd913fedabaad91d034a0216c5335792e8f5
marino.peter@gmail.com <marino.peter@gmail.com@7dcd178e-71d3-ac1e-a895-8dd51d562b40>
2013-05-02 12:11:44 +0000

    adding some tools to help test and debug

---
+++ trunk/SigmaUtil/SulEntity.h
@@ -0,0 +1,13 @@
+// SulEntity.h
+
+#ifndef __SULENTITY_H__
+#define __SULENTITY_H__
+
+#include <osg/referenced>
+
+class CSulEntity : public osg::Referenced
+{
+public:
+};
+
+#endif // __SULENTITY_H__
